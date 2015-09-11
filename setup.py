d1f84593a9a1dfa56ae6bf33bc1c71c81c4f55c0
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-09-11 16:12:12 +0200

    Update setup.py to allow local develop intallation
    setup.py now uses setuptools to allow the package to be installed
    locally in develop mode. This way the package can be used in, for
    example, ipython notebooks.

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-from distutils.core import setup
-from Cython.Build import cythonize
+from setuptools import setup, Extension
+from Cython.Distutils import build_ext
 import numpy as np
 
-setup(
-    ext_modules = cythonize("cptm/gibbs_inner.pyx"),
-    include_dirs=[np.get_include()]
-)
+gibbs_inner = Extension('gibbs_inner',
+                        sources=['cptm/gibbs_inner.pyx'])
+
+setup(name='cptm',
+      packages=['cptm'],
+      install_requires=['cython'],
+      ext_modules=[gibbs_inner],
+      cmdclass={'build_ext': build_ext},
+      include_dirs=[np.get_include()])
