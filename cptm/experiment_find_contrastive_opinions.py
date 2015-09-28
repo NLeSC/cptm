ad39a4f15ccc2ab404c95dc842fe2ba299451b78
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2015-09-28 17:11:47 +0200

    Update script to find contrastive opinions for topic words
    Add functionality to filter perspectives (so contrastive opinions can be
    calculated for a subset of perspectives) and add the possibility to
    specify the name of the output file.

--- cptm/experiment_find_contrastive_opinions.py
+++ cptm/experiment_find_contrastive_opinions.py
@@ -1,11 +1,12 @@
 import logging
 import pandas as pd
 import argparse
+import ast
 
 from cptm.utils.experiment import load_config, get_corpus, load_topics, \
     load_opinions, load_nks
 from cptm.utils.controversialissues import contrastive_opinions, \
-    jsd_opinions
+    jsd_opinions, filter_opinions
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(levelname)s : %(message)s', level=logging.INFO)
@@ -17,15 +18,34 @@ logging.getLogger('CPT_Gibbs').setLevel(logging.ERROR)
 parser = argparse.ArgumentParser()
 parser.add_argument('json', help='json file containing experiment '
                     'configuration.')
+parser.add_argument('--perspectives', '-p', help='list of perspectives to '
+                    'calculate contrastive opinions for')
+parser.add_argument('--output', '-o', help='file to save output to')
 args = parser.parse_args()
 
+print args.perspectives
+print args.output
+
 config = load_config(args.json)
+
+if args.output:
+    fName = args.output
+else:
+    fName = config.get('outDir').format('co_words_{}.csv'.
+                                        format(config.get('nTopics')))
+logger.info('writing output to {}'.format(fName))
+
 corpus = get_corpus(config)
 
 words = corpus.topic_words()
 topics = load_topics(config)
 opinions = load_opinions(config)
 nks = load_nks(config)
+
+if args.perspectives:
+    perspectives = ast.literal_eval(args.perspectives)
+    logger.info('filtering opinions to [{}]'.format(', '.join(perspectives)))
+    opinions = filter_opinions(perspectives, opinions)
 
 results = pd.DataFrame(index=words, columns=['jsd'])
 
@@ -39,7 +59,7 @@ for idx, word in enumerate(words):
                                                             len(words)))
 
 fName = 'co_words_{}.csv'.format(config.get('nTopics'))
-results.to_csv(config.get('outDir').format(fName), encoding='utf-8')
+results.to_csv(fName, encoding='utf-8')
 
 print 'top 20 words with most contrastive opinions'
 top = pd.Series(results['jsd'])
